c5ebd7569211063ed16c735cca8c4b0bfa238e6c
ivyoung926 <ivyoung926@gmail.com>
2018-02-02 18:38:20 +0000

    add in swagger for project

--- src/main/java/com/group9/columbus/Application.java
+++ src/main/java/com/group9/columbus/Application.java
@@ -1,11 +1,18 @@
 package com.group9.columbus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableAutoConfiguration
+@Import({springfox.documentation.spring.data.rest.configuration.
+SpringDataRestConfiguration.class})
 @SpringBootApplication
 public class Application {
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
