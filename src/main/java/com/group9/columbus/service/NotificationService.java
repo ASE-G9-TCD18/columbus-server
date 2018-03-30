7cdbee7f043bfe970160eead0082928551b13dc2
Amit Prasad <amitprasad53121@gmail.com>
2018-03-30 20:18:55 +0100

    API to reject join request completed

--- src/main/java/com/group9/columbus/service/NotificationService.java
+++ src/main/java/com/group9/columbus/service/NotificationService.java
@@ -12,32 +12,57 @@ import com.group9.columbus.notifications.FcmThread;
 public class NotificationService {
 
 	Logger logger = Logger.getLogger(NotificationService.class);
-	
+
 	@Autowired
 	ApplicationContext applicationContext;
-	
+
 	public boolean sendNewJoinRequestNotification(String fcmToken, String requestFrom) {
-		
+
 		String fcmMessage = null;
-		String title = "Request to join Trip by "+requestFrom;
+		String title = "Request to join Trip by " + requestFrom;
 		String body = null;
 		// Some logic to send notification
 		try {
 			fcmMessage = FcmMessageFactory.buildFcmMessage(fcmToken, title, body, null);
-			
-			if(fcmMessage == null) {
+
+			if (fcmMessage == null) {
 				logger.warn("Unable to send notification. Device Id Token not present.");
 				return false;
 			}
 			FcmThread fcmThread = applicationContext.getBean(FcmThread.class, fcmMessage);
-			//fcmThread.setFcmMessage(fcmMessage);
+			// fcmThread.setFcmMessage(fcmMessage);
 			fcmThread.start();
-			logger.info("Notification: Request to join group from ("+requestFrom+") sent successfully.");
+			logger.info("Notification: Request to join group from (" + requestFrom + ") sent successfully.");
 		} catch (Exception e) {
-			logger.error("Error sending notification requested by ("+requestFrom+").", e);
+			logger.error("Error sending notification requested by (" + requestFrom + ").", e);
 			return false;
 		}
-		
+
+		// If success then return true;
+		return true;
+	}
+
+	public boolean sendNotification(String fcmToken, String message) {
+
+		String fcmMessage = null;
+		String title = message;
+		String body = null;
+		// Some logic to send notification
+		try {
+			fcmMessage = FcmMessageFactory.buildFcmMessage(fcmToken, title, body, null);
+
+			if (fcmMessage == null) {
+				logger.warn("Unable to send notification. Device Id Token not present.");
+				return false;
+			}
+			FcmThread fcmThread = applicationContext.getBean(FcmThread.class, fcmMessage);
+			fcmThread.start();
+			logger.info("Notification: with message (" + title + ") sent successfully.");
+		} catch (Exception e) {
+			logger.info("Error sending Notification: with message (" + title + ").");
+			return false;
+		}
+
 		// If success then return true;
 		return true;
 	}
